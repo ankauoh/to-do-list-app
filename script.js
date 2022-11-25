bdc94ee5cc28ca9273befbb3110b4f6d8fe9c72a
ankauoh <andreas.kauderer@protonmail.com>
2022-11-25 12:27:49 +0100

    Styling and Delete Button Changes
    CSS: li > crossOutButton changed to li > button, index url stringified
    
    JS: crossOut functions moved within if statement. deleteListItem function added.

--- script.js
+++ script.js
@@ -9,21 +9,19 @@ function newItem() {
   } else {
     let list = $('#list')
     list.append(li);
+    li.on('dblclick', crossOut);
+    let crossOutButton = $('<button>X</button>');
+    li.append(crossOutButton);
+    crossOutButton.on('click', deleteListItem);
   }
 
   function crossOut() {
     li.toggleClass('strike');
   }
 
-  li.on('dblclick', crossOut);
-
-
-  let crossOutButton = $('<button>X</button>');
-  li.append(crossOutButton);
-
-  crossOutButton.on('click', deleteListItem);
-
-
+  function deleteListItem() {
+    li.addClass('delete');
+  }
 
   $('#list').sortable();
 }
